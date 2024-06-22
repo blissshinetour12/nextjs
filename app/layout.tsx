eab0f85268ab69f5ea1efdd3899f57d8b588f1f6
tawfeeqahmadkhan <tawfeeqpathan3@gmail.com>
2024-06-22 15:47:20 +0530

    add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,26 @@
 import type { Metadata } from 'next';
-
-import './globals.css'
+import './globals.css';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: 'Bliss Shine tour & travles',
-  description: 'Online booking for kashmir tour',
-}
+  title: 'Bliss Shine Tour & Travels',
+  description: 'Bliss Shine Tour & Travels is an online booking website for tours and travel services.',
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Bliss Shine Tour & Travels is an online booking website for tours and travel services." />
+        <title>Bliss Shine Tour & Travels</title>
+      </head>
       <body>
         <Navbar />
         <main className="relative overflow-hidden">
@@ -24,5 +29,5 @@ export default function RootLayout({
         <Footer />
       </body>
     </html>
-  )
+  );
 }
